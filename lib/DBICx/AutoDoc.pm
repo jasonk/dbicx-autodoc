f8190b6e778e4e346d83173be7366308340c048b
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2011-07-28 10:02:57 +0100

    Version 0.08

--- lib/DBICx/AutoDoc.pm
+++ lib/DBICx/AutoDoc.pm
@@ -1,7 +1,7 @@
 package DBICx::AutoDoc;
 use strict;
 use warnings;
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 use base qw( Class::Accessor::Grouped );
 use Carp qw( croak );
 use Template;
--- lib/DBICx/AutoDoc/Magic.pm
+++ lib/DBICx/AutoDoc/Magic.pm
@@ -1,7 +1,7 @@
 package DBICx::AutoDoc::Magic;
 use strict;
 use warnings;
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 use DBIx::Class::Relationship::Helpers;
 use base qw( DBIx::Class );
 
